e7483384ee50c09116fafb165b8b6f2c2fa73d66
Jagan Teki <jagan@amarulasolutions.com>
2016-10-30 23:53:33 +0530

    mtd: Add mtd core ops
    - Add generic mtd operations
    - Add mtd_dread|derase|dwrite
    
    The respetive MTD_UCLASS drivers must install the hooks to these
    mtd_ops and other core ops are act as a interface b/w drivers
    vs command code.
    
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/mtd/mtd-uclass.c
+++ drivers/mtd/mtd-uclass.c
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016 Jagan Teki <jagan@openedev.com>
  * Copyright (C) 2015 Thomas Chou <thomas@wytron.com.tw>
  *
  * SPDX-License-Identifier:	GPL-2.0+
@@ -8,6 +9,63 @@
 #include <dm.h>
 #include <errno.h>
 #include <mtd.h>
+#include <linux/log2.h>
+
+int mtd_dread(struct mtd *mtd, loff_t from, size_t len, size_t *retlen,
+	      u_char *buf)
+{
+	struct udevice *dev = mtd->dev;
+	const struct mtd_ops *ops = mtd_get_ops(dev);
+
+	if (!ops->read)
+		return -ENOSYS;
+
+	*retlen = 0;
+	if (from < 0 || from > mtd->size || len > mtd->size - from)
+		return -EINVAL;
+	if (!len)
+		return 0;
+
+	return ops->read(dev, from, len, retlen, buf);
+}
+
+int mtd_derase(struct mtd *mtd, loff_t addr, size_t len)
+{
+	struct udevice *dev = mtd->dev;
+	const struct mtd_ops *ops = mtd_get_ops(dev);
+
+	if (!ops->erase)
+		return -ENOSYS;
+
+	if (addr > mtd->size || len > mtd->size - addr)
+		return -EINVAL;
+	if (!(mtd->flags & MTD_WRITEABLE))
+		return -EROFS;
+	if (!len)
+		return 0;
+
+	return ops->erase(dev, addr, len);
+}
+
+int mtd_dwrite(struct mtd *mtd, loff_t to, size_t len, size_t *retlen,
+	       const u_char *buf)
+{
+	struct udevice *dev = mtd->dev;
+	const struct mtd_ops *ops = mtd_get_ops(dev);
+
+	if (!ops->write)
+		return -ENOSYS;
+
+	*retlen = 0;
+	if (to < 0 || to > mtd->size || len > mtd->size - to)
+		return -EINVAL;
+	if (!ops->write || !(mtd->flags & MTD_WRITEABLE))
+		return -EROFS;
+	if (!len)
+		return 0;
+
+	return ops->write(dev, to, len, retlen, buf);
+}
 
 /*
  * Implement a MTD uclass which should include most flash drivers.
@@ -17,5 +75,5 @@
 UCLASS_DRIVER(mtd) = {
 	.id		= UCLASS_MTD,
 	.name		= "mtd",
-	.per_device_auto_alloc_size = sizeof(struct mtd_info),
+	.per_device_auto_alloc_size = sizeof(struct mtd),
 };
--- include/mtd.h
+++ include/mtd.h
@@ -20,4 +20,57 @@ static inline struct mtd_info *mtd_get_info(struct udevice *dev)
 	return dev_get_uclass_priv(dev);
 }
 
+struct mtd {
+	struct udevice		*dev;		/* mtd device */
+	u32			flags;		/* mtd device flags */
+	uint64_t		size;		/* mtd device size */
+};
+
+struct mtd_ops {
+	int (*erase)(struct udevice *dev, loff_t addr, size_t len);
+	int (*read)(struct udevice *dev, loff_t from, size_t len,
+		    size_t *retlen, u_char *buf);
+	int (*write)(struct udevice *dev, loff_t to, size_t len,
+		     size_t *retlen, const u_char *buf);
+};
+
+/* Access the serial operations for a device */
+#define mtd_get_ops(dev) ((struct mtd_ops *)(dev)->driver->ops)
+
+/**
+ * mtd_dread() - read data from MTD device
+ *
+ * @mtd:	MTD device
+ * @from:	offset into device in bytes to read from
+ * @len:	length of bytes to read
+ * @retlen:	length of return bytes read to
+ * @buf:	buffer to put the data that is read
+ * @return 0 if OK, -ve on error
+ */
+int mtd_dread(struct mtd *mtd, loff_t from, size_t len, size_t *retlen,
+	      u_char *buf);
+
+/**
+ * mtd_dwrite() - write data to MTD device
+ *
+ * @mtd:	MTD device
+ * @to:		offset into device in bytes to write to
+ * @len:	length of bytes to write
+ * @retlen:	length of return bytes to write to
+ * @buf:	buffer containing bytes to write
+ * @return 0 if OK, -ve on error
+ */
+int mtd_dwrite(struct mtd *mtd, loff_t to, size_t len, size_t *retlen,
+	       const u_char *buf);
+
+/**
+ * mtd_derase() - erase blocks of the MTD device
+ *
+ * @mtd:	MTD device
+ * @to:		offset into device in bytes to erase to
+ * @len:	length of bytes to erase
+ * @return 0 if OK, -ve on error
+ */
+int mtd_derase(struct mtd *mtd, loff_t addr, size_t len);
+
 #endif	/* _MTD_H_ */
